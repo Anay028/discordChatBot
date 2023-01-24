b534c456cfb600d987141fb91d2d6f0e771328f8
Anay <anayparswani2008@gmail.com>
2023-01-24 23:42:29 +0530

    Attempting to fix Bug on hosting service

--- src/translate.js
+++ src/translate.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-// This package(google-translate-api) was been cloned from https://github.com/plainheart/google-translate-api due to some hosing issues with this package
-const translate = require('./google-translate-api');
-
-
-
+// // This package(google-translate-api) was been cloned from https://github.com/plainheart/google-translate-api due to some hosing issues with this package
+try{
+const translate = require('google-translate-api');
+} catch(err){
+const translate = require("@plainheart/google-translate-api")
+}
 module.exports = async function(query){
 translation = await translate(query, {to: 'en'})
 return translation.text
